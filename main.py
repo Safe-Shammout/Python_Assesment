76b1883acc79f23606ee11bbae66a573493570ca
Safe Shammout <>
2021-05-22 02:04:32 +0000

    Component 1: Trial 2: imported Image from PIL library, added image, worked on layout

--- main.py
+++ main.py
@@ -1,38 +1,43 @@
 from tkinter import *
-
+from PIL import ImageTk, Image
 
 #Componenet 1 (Game Starter Window object) will be constructed through following class
 class GameStarter:
     def __init__(self, parent):#constructor, The __init__() function is called automatically every time the class is being used to create a new object.
  
         background_color="dark blue"# to set it as background color for all the label widgets
+        #Background image on Frame
+        self.bg_image = Image.open("title.png") #need to use Image if need to resize 
+        self.bg_image = self.bg_image.resize((750, 650), Image.ANTIALIAS)
+        self.bg_image = ImageTk.PhotoImage(self.bg_image) 
 
-        #frame set up
-        self.quiz_frame=Frame(parent, bg = background_color, padx=100, pady=100)
-        #padx, pady How many pixels to pad widget, horizontally (x) and vertically (y), outside widget's borders.
-        self.quiz_frame.grid()#This geometry manager organizes widgets in a table-like structure in the parent widget.
-               
         #widgets goes below
-        self.heading_label=Label(self.quiz_frame, text="Game Name", font=("Tw Cen MT","18","bold"),bg=background_color)
-        self.heading_label.grid(row=0, column=2, padx=20) 
-    
-        
-        #label for username
-        self.user_label=Label(self.quiz_frame, text="Please enter your username below: ", font=("Tw Cen MT","16"),bg=background_color)
-        self.user_label.grid(row=1, column=2, padx=20, pady=20) 
-        
+        #frame set up
+        self.quiz_frame=Frame(parent, bg = background_color)
+        self.quiz_frame.grid()
+
+        #image label below
+        self.image_label= Label(self.quiz_frame, image=self.bg_image)
+        self.image_label.place(x=0, y=0, relwidth=1, relheight=1) # make label l to fit the frame
+
+        #label for spacing to fit image
+        self.space=Label(self.quiz_frame, bg="black")
+        self.space.grid(row=1,pady=50)
+
         #entry box
         self.entry_box=Entry(self.quiz_frame)
-        self.entry_box.grid(row=2, column=2,padx=20, pady=20)
-        
+        self.entry_box.grid(row=2, column=2,padx=120, pady=200)
+     
         #continue button
         self.continue_button = Button(self.quiz_frame, text="Continue", font=("Helvetica", "13", "bold"), bg="lawn green", command=self.name_collection)
-        self.continue_button.grid(row=3, column=3,  padx=20, pady=20) 
+        self.continue_button.grid(row=3, column=3,  padx=10, pady=10) 
 
          #cancel button
         self.cancel_button = Button(self.quiz_frame, text="Cancel", font=("Helvetica", "13", "bold"), bg="red", command=self.name_collection)
         self.cancel_button.grid(row=3,column=1,  padx=10, pady=10)        
 
+        self.space=Label(self.quiz_frame)
+        self.space.grid(row=10)
 
     def name_collection(self):
         name=self.entry_box.get()
