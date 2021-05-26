a62a37dd953d013fc517fa6a77cbb083ecd1556a
Safe Shammout <>
2021-05-26 07:33:00 +0000

    Component 2: Trial 1: display the story using a label and it works good

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from tkinter import *
 from PIL import ImageTk, Image
 
+
+#variables
+names_list = []
+
+
 #Componenet 1 (Game Starter Window object) will be constructed through following class
 class GameStarter:
     def __init__(self, parent):#constructor, The __init__() function is called automatically every time the class is being used to create a new object.
@@ -14,34 +19,60 @@ class GameStarter:
         #widgets goes below  
 
         #image label below
-        self.image_label= Label(parent, image=self.bg_image)
+        self.image_label = Label(parent, image=self.bg_image)
         self.image_label.place(x=0, y=0, relwidth=1, relheight=1) # make label l to fit the frame
 
         #entry box
-        self.entry_box=Entry(parent)
-        self.entry_box.place(x=350, y=475)
+        self.entry_box = Entry(parent)
+        self.entry_box.place(x=350, y=400)
      
         #continue button
         self.continue_button = Button(parent, text="Continue", font=("Helvetica", "13", "bold"), bg="lawn green", command=self.name_collection)
-        self.continue_button.place(x=650, y=650)
+        self.continue_button.place(x=550, y=550)
 
          #cancel button
         self.cancel_button = Button(parent, text="Cancel", font=("Helvetica", "13", "bold"), bg="red")
-        self.cancel_button.place(x=50, y=650) 
+        self.cancel_button.place(x=50, y=550) 
 
 
     def name_collection(self):
         name=self.entry_box.get()
-        names.append(name) #add name to names list declared at the beginning
+        names_list.append(name) #add name to names list declared at the beginning
         self.image_label.destroy()
         self.entry_box.destroy()
         self.continue_button.destroy()
         self.cancel_button.destroy()
+        StoryWindow(root)
+
+#Componenet 2 (Story wimdow object) will be constructed through following class
+
+
+
+class StoryWindow:
+  def __init__(self, parent):
+
+    background_color = "blue"
+  
+    #frame to place widgets on
+    self.story_frame = Frame (parent, bg = background_color)
+    self.story_frame.grid()
+    self.story1_label = Label( self.story_frame, text="Testing how long the story can be I wan to write a lot to see if a label can handle too much writing,\n \n\n so if my story is big, like few lines big, it will show or not on the frame?????\n\n\n")
+    self.story1_label.grid(row=1, padx=50, pady=50)
+
+
+
+
+
 
 #Program runs below
 if __name__ == "__main__":
-    root = Tk()
-    root.title("Game Name")
-    root.geometry("800x800")
-    game_starter_window = GameStarter(root) #instantiation, making an instance (object) of the class
-    root.mainloop()#so the window doesnt dissapear
+   root = Tk()
+   root.title("Game Name")
+   root.geometry("750x650")
+   game_starter_window = GameStarter(root) #instantiation, making an instance (object) of the class
+   root.mainloop()#so the window doesnt dissapear
+
+
+
+
+
